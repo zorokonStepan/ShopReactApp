71ea3b607c465c97cedead66268d7756fe64cb7f
SKolodkin <dev1@instrument-fit.ru>
2023-04-24 08:39:39 +0300

    create Header and Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Footer() {
+    return (
+        <footer>
+            Все права защищены &copy;
+        </footer>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Header() {
+    return (
+        <header>
+            <div>
+                <span className='logo'>House Staff</span>
+            </div>
+            <div className='presentation'>
+
+            </div>
+        </header>
+    )
+}
